676f5ad6264c43f9318ee9a3aaee7ae7bc614b56
kimja01 <kimja7045@naver.com>
2019-11-29 19:21:45 +0900

    첫 부분 로그인 됩니다~다들 파이팅합시다

--- src/cafe/CafeSystem.java
+++ src/cafe/CafeSystem.java
@@ -19,6 +19,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
@@ -32,6 +33,7 @@ import cafe.OrderSystem.SongScreenPanel;
 import cafe.AdminGUI;
 
 import cafe.Menu;
+import cafe.AdminGUI;
 
 public class CafeSystem extends JFrame {
 	private Image screenImage; // 더블 버퍼링을 위해서 전체 화면에 대한
@@ -40,18 +42,17 @@ public class CafeSystem extends JFrame {
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
 	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
 	private ImageIcon menuBarImage = new ImageIcon(Main.class.getResource("../images/menuBar.png"));
-	
 
 	private int mouseX, mouseY;
-	
+
 	private CardLayout cards = new CardLayout();
 
 	OrderSystem OS = new OrderSystem(this);
-	
+
 	public CafeSystem() {
 		setTitle("Cafe Management System"); // 프로그램 이름
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT); // 프로그램 창 설정
-		Container c= getContentPane();
+		Container c = getContentPane();
 		c.setLayout(cards);
 		c.add("Login", new LoginScreenPanel());
 		c.add("Main", new MainScreenPanel());
@@ -63,10 +64,11 @@ public class CafeSystem extends JFrame {
 		setBackground(new Color(0, 0, 0, 0)); // 배경이 회색이 아니라 전부 하얀색으로 바뀜
 
 	}
+
 	class MainScreenPanel extends JPanel {
 		private Image background = new ImageIcon(Main.class.getResource("../images/CafeBackground.jpg")).getImage();
 		private ImageIcon playbutton = new ImageIcon(Main.class.getResource("../images/play.png"));
-		private ImageIcon pausebutton = new ImageIcon(Main.class.getResource("../images/pause.png")); 
+		private ImageIcon pausebutton = new ImageIcon(Main.class.getResource("../images/pause.png"));
 		private ImageIcon orderbutton = new ImageIcon(Main.class.getResource("../images/order1.png"));
 		private ImageIcon orderbutton1 = new ImageIcon(Main.class.getResource("../images/order2.png"));
 		private ImageIcon orderbutton2 = new ImageIcon(Main.class.getResource("../images/order3.png"));
@@ -74,12 +76,16 @@ public class CafeSystem extends JFrame {
 		private ImageIcon managerbutton1 = new ImageIcon(Main.class.getResource("../images/manager2.png"));
 		private ImageIcon managerbutton2 = new ImageIcon(Main.class.getResource("../images/manager3.png"));
 		private ImageIcon Onbutton = new ImageIcon(Main.class.getResource("../images/on.png"));
-		private ImageIcon Offbutton = new ImageIcon(Main.class.getResource("../images/off.png")); 
-		
+		private ImageIcon Offbutton = new ImageIcon(Main.class.getResource("../images/off.png"));
+
 		private JLabel menuBar = new JLabel(menuBarImage);
 		private JButton exitButton = new JButton(exitButtonBasicImage);
+
 		public MainScreenPanel() {
-			orderbutton = new ImageIcon(orderbutton.getImage().getScaledInstance(350, 130, Image.SCALE_SMOOTH));		//이미지 아이콘 크기 조절
+			orderbutton = new ImageIcon(orderbutton.getImage().getScaledInstance(350, 130, Image.SCALE_SMOOTH)); // 이미지
+																													// 아이콘
+																													// 크기
+																													// 조절
 			orderbutton1 = new ImageIcon(orderbutton1.getImage().getScaledInstance(350, 130, Image.SCALE_SMOOTH));
 			orderbutton2 = new ImageIcon(orderbutton2.getImage().getScaledInstance(350, 130, Image.SCALE_SMOOTH));
 			managerbutton = new ImageIcon(managerbutton.getImage().getScaledInstance(350, 130, Image.SCALE_SMOOTH));
@@ -89,21 +95,21 @@ public class CafeSystem extends JFrame {
 			pausebutton = new ImageIcon(pausebutton.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
 			Onbutton = new ImageIcon(Onbutton.getImage().getScaledInstance(96, 40, Image.SCALE_SMOOTH));
 			Offbutton = new ImageIcon(Offbutton.getImage().getScaledInstance(96, 40, Image.SCALE_SMOOTH));
-			setUndecorated(true);	// 기존 메뉴바를 감춰줌
+			setUndecorated(true); // 기존 메뉴바를 감춰줌
 			setLayout(null);
 			add(exitButton);
 			add(menuBar);
 			MenuBar(menuBar, exitButton);
-			ListPanel(this,580,220,"<노래예약>");		//노래 예약 목록 판넬
-			ListPanel(this,930,220,"<주문내역>");		//주문 내역 목록 판넬
-			
-			JPanel songPanel = new JPanel();		//노래 재생목록 판넬
-			songPanel.setBounds(820,120,400,40);
-			songPanel.setBackground(new Color(255,255,255,120));
+			ListPanel(this, 580, 220, "<노래예약>"); // 노래 예약 목록 판넬
+			ListPanel(this, 930, 220, "<주문내역>"); // 주문 내역 목록 판넬
+
+			JPanel songPanel = new JPanel(); // 노래 재생목록 판넬
+			songPanel.setBounds(820, 120, 400, 40);
+			songPanel.setBackground(new Color(255, 255, 255, 120));
 			songPanel.setLayout(null);
 			add(songPanel);
-			
-			JButton songbutton = new JButton(playbutton);		//노래 재생버튼
+
+			JButton songbutton = new JButton(playbutton); // 노래 재생버튼
 			songbutton.setBorderPainted(false);
 			songbutton.setContentAreaFilled(false);
 			songbutton.setFocusPainted(false);
@@ -111,23 +117,24 @@ public class CafeSystem extends JFrame {
 			songbutton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					
+
 					// TODO Auto-generated method stub
-					if(songbutton.getIcon() == playbutton)
+					if (songbutton.getIcon() == playbutton) { // > 모양의 노래 재생 버튼을 눌렀을 때
 						songbutton.setIcon(pausebutton);
-					else if(songbutton.getIcon() == pausebutton)
+
+					} else if (songbutton.getIcon() == pausebutton)
 						songbutton.setIcon(playbutton);
 				}
-				});
+			});
 			add(songbutton);
-			
-			JLabel TitleLabel = new JLabel("Cafe Mangerment");		//타이틀 라벨
-			TitleLabel.setFont(new Font("Verdana",Font.BOLD,60));
+
+			JLabel TitleLabel = new JLabel("Cafe Mangerment"); // 타이틀 라벨
+			TitleLabel.setFont(new Font("Verdana", Font.BOLD, 60));
 			TitleLabel.setForeground(Color.WHITE);
 			TitleLabel.setBounds(50, 55, 600, 80);
 			add(TitleLabel);
 
-			JButton Orderbutton = new JButton(orderbutton);		//주문 화면 버튼
+			JButton Orderbutton = new JButton(orderbutton); // 주문 화면 버튼
 			Orderbutton.setBorderPainted(false);
 			Orderbutton.setContentAreaFilled(false);
 			Orderbutton.setFocusPainted(false);
@@ -139,10 +146,10 @@ public class CafeSystem extends JFrame {
 				public void actionPerformed(ActionEvent e) {
 					OS.setVisible(true);
 				}
-				});
+			});
 			add(Orderbutton);
-			
-			JButton Managerbutton = new JButton(managerbutton);		//관리 화면 버튼
+
+			JButton Managerbutton = new JButton(managerbutton); // 관리 화면 버튼
 			Managerbutton.setBorderPainted(false);
 			Managerbutton.setContentAreaFilled(false);
 			Managerbutton.setFocusPainted(false);
@@ -154,9 +161,9 @@ public class CafeSystem extends JFrame {
 				public void actionPerformed(ActionEvent e) {
 					new AdminGUI();
 				}
-				});
+			});
 			add(Managerbutton);
-			
+
 			JButton OnOffbutton = new JButton(Onbutton);
 			OnOffbutton.setBorderPainted(false);
 			OnOffbutton.setContentAreaFilled(false);
@@ -165,37 +172,38 @@ public class CafeSystem extends JFrame {
 			OnOffbutton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					
+
 					// TODO Auto-generated method stub
-					if(OnOffbutton.getIcon() == Offbutton) {
+					if (OnOffbutton.getIcon() == Offbutton) {
 						OnOffbutton.setIcon(Onbutton);
 						OS.OrderSystemEnable(1);
-					}
-					else if(OnOffbutton.getIcon() == Onbutton) {
+					} else if (OnOffbutton.getIcon() == Onbutton) {
 						OnOffbutton.setIcon(Offbutton);
 						OS.OrderSystemEnable(0);
 					}
 				}
-				});
+			});
 			add(OnOffbutton);
-			
+
 		}
+
 		public void paintComponent(Graphics g) { // 초기화면 배경
 			g.drawImage(background, 0, 0, Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, null);
 			setOpaque(false);
 			super.paintComponent(g);
 		}
 	}
-	
+
 	class LoginScreenPanel extends JPanel {
 		private Image background = new ImageIcon(Main.class.getResource("../images/CafeBackground.jpg")).getImage();
-		
+
 		private JLabel menuBar = new JLabel(menuBarImage);
 		private JButton exitButton = new JButton(exitButtonBasicImage);
-		
+
 		private JPasswordField passText = new JPasswordField(20);
-	    private JTextField userText = new JTextField(20);
-	    
+		private JTextField userText = new JTextField(20);
+		private int result;
+		
 		public LoginScreenPanel() {
 			setUndecorated(true);	// 기존 메뉴바를 감춰줌
 			setLayout(null);
@@ -223,34 +231,59 @@ public class CafeSystem extends JFrame {
 			TitleLabel.setBounds(50, 55, 600, 80);
 			add(TitleLabel);
 			
-			JButton LoginButton = new JButton("로그인");
-			LoginButton.setBounds(220, 34, 75, 75);
-			LoginButton.addActionListener(new ActionListener() {
+			JButton btnLogin = new JButton("로그인");
+			btnLogin.setBounds(220, 34, 75, 75);
+			btnLogin.addActionListener(new ActionListener() { 			// 로그인 버튼 이벤트 처리
 				@Override
 				public void actionPerformed(ActionEvent e) {
-				
-					ChangePanel("Main");
+					dbLogin();		
+					if(result!=0 && result!=-1)
+						ChangePanel("Main");
 				}
 			});
-			LP.add(LoginButton);
+			LP.add(btnLogin);
 		}
+		// 로그인 기능 수행하는 dbLogin() 메서드 정의
+		 void dbLogin() {
+			// DB 아이디와 패스워드를 가져와서 DAO 클래스의 login() 메서드 호출
+			// => 로그인 수행 결과를 리턴받아 "아이디 없음", "패스워드 틀림", "로그인 성공" 세 가지로 분류
+			// 0. 로그인 버튼 vs 로그아웃 버튼 판별
+			// 1. 로그인 버튼일 경우
+			// 1-1. 아이디 또는 패스워드가 잘못됐을 경우 경고메세지 출력 후 해당 필드에 커서 요청
+			//Username, Password 텍스트필드 입력 가능 설정
+			
+			String username = new String(userText.getText());
+			String password = new String(passText.getPassword());
+				
+				AdminDTO dto = new AdminDTO(username, password);
+				AdminDAO dao = AdminDAO.getInstance();
+				result = dao.login(dto);
+
+				if (result == 0) { // 아이디가 없을 경우
+					JOptionPane.showMessageDialog(rootPane, "존재하지 않는 계정입니다.", "로그인 오류", JOptionPane.ERROR_MESSAGE);
+					userText.requestFocus();
+					return;
+				} else if (result == -1) { // 패스워드가 틀렸을 경우
+					JOptionPane.showMessageDialog(rootPane, "패스워드가 일치하지 않습니다.", "로그인 오류", JOptionPane.ERROR_MESSAGE);
+					passText.requestFocus();
+					return;
+				}	 
+			}
 		class LoginPanel extends JPanel {
 			public LoginPanel() {
-				setBackground(new Color(255,255,255,120));
+				setBackground(new Color(255, 255, 255, 120));
 				setLayout(null);
 			}
 		}
+
 		public void paintComponent(Graphics g) { // 초기화면 배경
 			g.drawImage(background, 0, 0, Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, null);
 			setOpaque(false);
 			super.paintComponent(g);
 		}
 	}
-	
-	
-	
-	
-	public void MenuBar(JLabel menuBar, JButton exitButton) {		//메뉴바 생성
+
+	public void MenuBar(JLabel menuBar, JButton exitButton) { // 메뉴바 생성
 		menuBar.setBounds(0, 0, 1280, 30);
 		menuBar.addMouseListener(new MouseAdapter() {
 			@Override
@@ -277,42 +310,43 @@ public class CafeSystem extends JFrame {
 				exitButton.setIcon(exitButtonEnteredImage); // 마우스가 올라가을때 enterImage로 변경
 				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // 마우스가 올라갔을 때 손모양으로 변경
 			}
-	
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				exitButton.setIcon(exitButtonBasicImage);
 				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
-	
+
 			public void mousePressed(MouseEvent e) {
 				System.exit(0);
 			}
 		});
 	}
-	
-	public void ListPanel(JPanel p,int x, int y, String s) {	//리스트 판넬 - 추가할 판넬. 위치x, 위치y, 초기화버튼이름
+
+	public void ListPanel(JPanel p, int x, int y, String s) { // 리스트 판넬 - 추가할 판넬. 위치x, 위치y, 초기화버튼이름
 		JPanel OrderPanel = new JPanel();
 		JScrollPane scroll = new JScrollPane(OrderPanel);
 		OrderPanel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.PAGE_END;
+		gbc.anchor = GridBagConstraints.PAGE_END;
 		gbc.weightx = 1.0;
 		gbc.weighty = 1.0;
 		gbc.gridx = 0;
 		gbc.gridy = 1000;
-		OrderPanel.add(new JLabel(s),gbc);
+		OrderPanel.add(new JLabel(s), gbc);
 		gbc.weighty = 0;
 		gbc.anchor = GridBagConstraints.PAGE_START;
-		scroll.setBounds(x,y,300,400);
-		scroll.setBackground(new Color(0,0,0,0));
-		OrderPanel.setBorder(new TitledBorder(new LineBorder((new Color(255,255,255,255)),3)));
-		OrderPanel.setBackground(new Color(255,255,255,122));
+		scroll.setBounds(x, y, 300, 400);
+		scroll.setBackground(new Color(0, 0, 0, 0));
+		OrderPanel.setBorder(new TitledBorder(new LineBorder((new Color(255, 255, 255, 255)), 3)));
+		OrderPanel.setBackground(new Color(255, 255, 255, 122));
 		p.add(scroll);
 	}
-	public void addorder(Menu [] m) {
+
+	public void addorder(Menu[] m) {
 	}
-	
+
 	public void ChangePanel(String S) {
 		cards.show(this.getContentPane(), S);
 	}
